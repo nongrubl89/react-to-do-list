fa8f28acb1eacdb6e0b38beb26cd3282fb3d027c
Lisa <lisaburgnon@gmail.com>
2021-05-19 18:51:25 -0700

    changed shape of jumbotron

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,23 +3,29 @@ import { Navbar, Nav } from "react-bootstrap";
 import ToDoForm from "./ToDoForm";
 import { AuthContext } from "../services/context.js";
 import { app } from "../services/config";
+import Jtron from "./Jtron";
 
 export default function Navigation() {
   const { user } = useContext(AuthContext);
-  const u = !!user;
-  console.log(u);
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar expand="lg">
         <Navbar.Brand bg="dark">To-Do List</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
           {!!user ? (
             <>
-              <Nav.Link>{user.displayName}</Nav.Link>
-              <Nav.Link onClick={() => app.auth().signOut()}>Sign Out</Nav.Link>
+              <Nav.Link className="text-dark">{user.displayName}</Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                onClick={() => app.auth().signOut()}
+              >
+                Sign Out
+              </Nav.Link>
             </>
           ) : (
-            <Nav.Link href="/signin">Sign In</Nav.Link>
+            <Nav.Link className="text-dark" href="/signin">
+              Sign In
+            </Nav.Link>
           )}
         </Navbar.Collapse>
       </Navbar>
